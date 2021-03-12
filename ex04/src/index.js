cfcb36f0fbe951dcb5ab0d2c77951edfe63a3395
demon034 <sspahic2@etf.unsa.ba>
2021-03-12 10:53:37 +0100

    Everything

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    var myDecimal = 8.2;
+    var myDecimalE = 0.00254
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+
+module.exports = main;

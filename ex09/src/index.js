2f45fdd3c2f05d316b5b8bdc04c3a2e4a423d63f
demon034 <sspahic2@etf.unsa.ba>
2021-03-12 20:30:32 +0100

    Finished exercise 9

---
+++ ex09/src/index.js
@@ -0,0 +1,26 @@
+function main(){
+
+    // only change code below this line
+
+    var firstNum = 6;
+    var secondNum = 25;
+    var thirdNum = 5.2;
+
+    firstNum *= 9;
+    thirdNum *= 10;
+    secondNum /= 5;
+
+    // Only change code above this line
+
+    return {
+        firstNum,
+
+        secondNum,
+
+        thirdNum
+    };
+}
+
+console.log(main());
+
+module.exports = main;
